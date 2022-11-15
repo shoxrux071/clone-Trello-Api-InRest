3a3658aa48ff4a2b8bb757a1292601a466d462d5
shoxrux071 <begratefuller071@gmail.com>
2022-11-15 22:30:51 +0500

    initializing project

---
+++ src/main/java/uz/shox/clonetrelloapiinrest/CloneTrelloApiInRestApplication.java
@@ -0,0 +1,13 @@
+package uz.shox.clonetrelloapiinrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneTrelloApiInRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneTrelloApiInRestApplication.class, args);
+    }
+
+}
